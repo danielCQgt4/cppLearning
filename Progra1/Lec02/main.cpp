ff94ea36195aca89eeacd59579a44faa24cb35ef
Daniel Coto Quiros <danielcotoquiros@hotmail.com>
2021-03-04 17:38:28 -0600

    Feat: fix lec 02

--- Progra1/Lec02/main.cpp
+++ Progra1/Lec02/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 //Como leer el .length de un arreglo
 int main() {
     int arr[] = {0, 1, 1};
-    int a = 0x7ffdf366ff80;
-    int b = 0x7ffdf366ff98;
+    long int a = 0x7ffdf366ff80;
+    long int b = 0x7ffdf366ff98;
     cout << arr << " | " << *arr << " | " << &arr << endl;
     cout << "Resta " << (b - a) << endl;
     cout << sizeof(arr) << " | " << sizeof(*arr) << " | " << (&arr) << " | " << (&arr + 1) << endl;
